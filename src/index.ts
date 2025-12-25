93cdd23d835fce9f42d0dbc566f13697e6192cd1
sh7t <karimgaldonsky@gmail.com>
2025-12-25 23:11:52 +0200

    upd: fifth homework uploaded (1/2)

--- src/index.ts
+++ src/index.ts
@@ -1,230 +1,223 @@
-import promptSync from 'prompt-sync';
-import bands from './bandsDiff.json'
+// import promptSync from 'prompt-sync';
+import { names } from './names';
 
+function roundCurrency(value: number): number {
+    return Math.round(value * 100) / 100;
+}
 
-type MusicBand = {
-    name: string;
-    genre: string;
-    originCountry: string;
-    foundedYear: number;
-    isStillActive: boolean;
-    trackCount: number;
-    albumCount: number;
-    members: BandMember[];
-    albums: Album[];
-};
-
-type BandMember = {
-    name: string;
-    instrument: string;
-    birthYear: number;
-    joinedYear: number;
-    leftYear?: number;
-};
-
-type Album = {
-    id: number;
-    title: string;
-    releaseYear: number;
-    tracks: Track[];
-};
-
-type Track = {
-    id: number;
-    title: string;
-    durationInSeconds: number;
-    releaseYear: number;
-};
-
-
-function isLengthSmallerThanNum(str: string, num: number) {
-    return str.trim().length < num;
-
+class Random {
+    static randomInteger(min: number, max: number): number {
+        if (min > max) { throw new Error("Minimum value can't be greater than maximum!"); }
+        return Math.floor(Math.random() * (max-min)) + min;
+    }
+}
+class ValidationHelper {
+    static isPositive(value: number): boolean { return value > 0; }
+    static isNegative(value: number): boolean { return value < 0; }
+    static isNonPositive(value: number): boolean { return value <= 0; }
+    static isNonNegative(value: number): boolean { return value >= 0; }
 }
 
 // 1.
-class Car {
-    private readonly id: number;
-    public brand: string;
-    public year: number;
-    private mileage: number;
-    private gasolineCapacity: number;
-    private readonly fuelConsumption: number;
+abstract class Person {
+    // fields
+    protected readonly _defaultHours: number = 160;
+    protected _totalHours: number = 0;
+    protected _hours: number = 0;
+    protected _name: string = "";
+    protected _balance: number = 0;
+    protected _salaryPerHour: number = 30;
+    protected _coefficient: number = 1;
+    protected _overtimeCoefficient: number = 1;
 
-    constructor(brand: string, year: number, fuelConsumption: number) {
-        if (isLengthSmallerThanNum(brand, 3))
-            throw new Error("Brand name can't be that short.");
+    // init
+    protected constructor(name: string) {
+        this.name = name;
+    }
 
-        if (year > new Date().getFullYear())
-            throw new Error("Year has to be a real year.");
 
-        if (fuelConsumption < 0)
-            throw new Error("FuelConsumption has to be a positive number.");
-
-        this.brand = brand.trim();
-        this.year = year;
-        this.mileage = 0;
-        this.gasolineCapacity = 0;
-        this.fuelConsumption = fuelConsumption;
+    // get-n-set
+    get totalHours(): number {
+        return this._totalHours;
     }
-    get getId() {
-        return this.id;
-    }
-    get getMileage() {
-        return this.mileage;
-    }
-    get getGas() {
-        return this.gasolineCapacity;
-    }
-    set setGas(value: number) {
-        if (value < 0) {
-            return;
+    protected set totalHours(value: number) {
+        if (ValidationHelper.isNonPositive(value)) {
+            throw new Error("Total hours must be greater than zero.");
         }
-        this.gasolineCapacity = value;
+        this._totalHours = value;
+    }
+    get hours(): number {
+        return this._hours;
+    }
+    protected set hours(value: number) {
+        if (ValidationHelper.isNegative(value)) {
+            throw new Error("Hours must be greater than zero.");
+        }
+        this._hours = value;
     }
 
-    drive(km: number) {
-        if (km < 0)
-            throw new Error("Wrong kilometers value.");
-        return km * this.fuelConsumption;
-    }
-}
-
-// 2.
-class Comment {
-    private readonly id: number;
-    private author: string;
-    private text: string;
-    private advantages: string[];
-    private disadvantages: string[];
-    private createdAt: Date;
-    private grade: number;
-    private likes: number;
-    private dislikes: number;
-
-    constructor(author: string, text: string, grade: number, advantages?: string[], disadvantages?: string[]) {
-        if (isLengthSmallerThanNum(author, 2))
-            throw new Error("Invalid name.");
-        if (isLengthSmallerThanNum(text, 20))
-            throw new Error("Invalid comment text.");
-        if (grade < 0 || grade > 5)
-            throw new Error("Invalid grade.");
-
-
-        this.author = author.trim();
-        this.text = text.trim();
-        this.advantages = advantages;
-        this.disadvantages = disadvantages;
-        this.createdAt = new Date();
-        this.grade = grade;
-    }
-
-    // гетери-сетери...
-    // ...
-    // ...
-}
-const COMMENT_TEXT: string = "Чудовий подарунок, як для дитини, так і для дорослого. Приємним бонусом було, що це не лише" +
-    "конструктор, а і світильник. Прийшло вчасно та з усіма деталями";
-const COMMENT_AUTHOR: string = "Валентина Омельчук"
-
-let comment: Comment = new Comment(COMMENT_AUTHOR, COMMENT_TEXT, 5, ["Приємна ціна", "Цікавий варіант подарунку"]);
-
-
-// 3.
-enum RoomTypes {
-    Single,
-    Double,
-    Suite
-}
-
-class Room {
-    private readonly _id: number;
-    private _roomType: RoomTypes;
-    private _pricePerNight: number;
-    private _availability: boolean;
-    private _floor: number;
-
-    constructor(roomType: RoomTypes, floor: number, pricePerNight: number) {
-        if (pricePerNight < 0)
-            throw new Error("Invalid price per night.");
-        if (floor < 0)
-            throw new Error("Invalid floor.");
-
-        this._floor = floor;
-        this._roomType = roomType;
-        this._pricePerNight = pricePerNight;
-        this._availability = true;
-    }
-
-    get id() {
-        return this._id;
-    }
-    get roomType() {
-        return this._roomType;
-    }
-    get pricePerNight() {
-        return this._pricePerNight;
-    }
-    set pricePerNight(pricePerNight: number) {
-        if (pricePerNight < 0)
-            throw new Error("Invalid price per night.");
-        this._pricePerNight = pricePerNight;
-    }
-    get availability() {
-        return this._availability;
-    }
-    changeAvailability() {
-        this._availability = !this._availability;
-    }
-    get floor() {
-        return this._floor;
-    }
-}
-class Hotel {
-    private readonly _id: number;
-    private _name: string;
-    private _rating: number;
-    private _rooms: Room[]
-
-    constructor(name: string, rating: number, rooms: Room[]) {
-        if (rooms.length === 0)
-            throw new Error("No rooms in hotel.");
-
-        this.name = name.trim();
-        this.rating = rating;
-        this._rooms = rooms;
-    }
-
-    get id() {
-        return this._id;
-    }
-    get name() {
+    get name(): string {
         return this._name;
     }
-    set name(name: string) {
-        if (isLengthSmallerThanNum(name, 2))
-            throw new Error("Invalid name.");
-        this._name = name;
-    }
-    get rating() {
-        return this._rating;
-    }
-    set rating(rating: number) {
-        if (rating < 0 )
-            throw new Error("Invalid rating.")
-        this._rating = rating;
-    }
-    book(roomType: RoomTypes, floor?: number) {
-        let room: Room;
-
-        if (floor) {
-            room = this._rooms.find((room: Room) => room.floor === floor && room.roomType === roomType);
-        } else {
-            room = this._rooms.find((room: Room) => room.roomType === roomType);
+    protected set name(value: string) {
+        if (value.trim().length < 2) {
+            throw new Error("Name must be at least 2 characters.");
         }
+        this._name = value.trim();
+    }
 
-        if (room === undefined)
-            throw new Error("No room with such parameters found.");
-        return room;
+    get balance(): number {
+        return this._balance;
+    }
+    protected set balance(value: number) {
+        if (ValidationHelper.isNegative(value)) {
+            throw new Error("Balance must be a positive number.");
+        }
+        this._balance = value;
+    }
+
+    get salaryPerHour(): number {
+        return this._salaryPerHour;
+    }
+    protected set salaryPerHour(value: number) {
+        if (ValidationHelper.isNonPositive(value)) {
+            throw new Error("Salary per hour must be greater than zero.");
+        }
+        this._salaryPerHour = value;
+    }
+
+    get coefficient() {
+        return this._coefficient;
+    }
+
+    get overtimeCoefficient() {
+        return this._overtimeCoefficient;
+    }
+
+    protected get defaultHours() {
+        return this._defaultHours;
+    }
+
+    // functions
+    abstract calculateSalary(hours: number): number;
+
+    receiveSalary(salary: number): void {
+        if (ValidationHelper.isNonPositive(salary)) { throw new Error("Salary must be greater than zero."); }
+        this.balance += salary;
+    };
+
+    addHours(hours: number) {
+        if (ValidationHelper.isNonPositive(hours)) { throw new Error("Accruing hours must be greater than zero."); }
+        this.hours += hours;
+    }
+
+    resetHours(): void {
+        this.totalHours += this.hours;
+        this.hours = 0;
     }
 }
+class Professor extends Person {
+    private static autoInc: number = 1;
+    public readonly id: number;
+    private allowance: number = 2000;
+    protected _coefficient: number = 3;
+    protected _overtimeCoefficient: number = 5;
+
+    constructor(name: string) {
+        super(name);
+        this.id = Professor.autoInc++;
+    }
+
+    calculateSalary(hours: number = this.hours) {
+        if ( ValidationHelper.isNonPositive(hours) ) { throw new Error("Professor's hours must be greater than zero."); }
+        if (hours <= this._defaultHours) { return roundCurrency((this.salaryPerHour * this.coefficient * hours) + this.allowance); }
+        return roundCurrency((this.salaryPerHour * this.coefficient * this.defaultHours) + (this.salaryPerHour * this._overtimeCoefficient * (hours - this.defaultHours)) + this.allowance);
+    }
+}
+class Student extends Person {
+    private static autoInc: number = 1;
+    public readonly id: number;
+    protected _coefficient: number = 0.5;
+
+    constructor(name: string) {
+        super(name);
+        this.id = Student.autoInc++;
+    }
+
+    calculateSalary(hours: number = this.hours) {
+        if ( ValidationHelper.isNonPositive(hours) ) { throw new Error("Student's hours must be greater than zero."); }
+        const randomExpense: number = Random.randomInteger(-700, 700);
+        if (hours <= this._defaultHours) { return roundCurrency((this.salaryPerHour * this.coefficient * hours) + randomExpense); }
+        return roundCurrency((this.salaryPerHour * this.coefficient * this.defaultHours) + (this.salaryPerHour * this._overtimeCoefficient * (hours - this.defaultHours)) + randomExpense);
+    }
+}
+class Staff extends Person {
+    private static autoInc: number = 1;
+    public readonly id: number;
+    protected _coefficient: number = 1.2;
+    protected _overtimeCoefficient: number = 2;
+
+    constructor(name: string) {
+        super(name);
+        this.id = Staff.autoInc++;
+    }
+
+    calculateSalary(hours: number = this.hours): number {
+        if ( ValidationHelper.isNonPositive(hours) ) { throw new Error("Staff's hours must be greater than zero."); }
+        if (hours <= this._defaultHours) { return roundCurrency((this.salaryPerHour * this.coefficient * hours)); }
+        return roundCurrency((this.salaryPerHour * this.coefficient * this.defaultHours) + (this.salaryPerHour * this._overtimeCoefficient * (hours - this.defaultHours)));
+    }
+}
+class LifeImitator {
+    generateProfessor(): Professor { return new Professor(names[Random.randomInteger(0, names.length)]); }
+    generateProfessors(arrayLength: number): Professor[] {
+        if (ValidationHelper.isNonPositive(arrayLength)) { throw new Error("Amount of professors must be greater than zero."); }
+        let professors: Professor[] = [];
+        for (let i = 0; i < arrayLength; i++) {
+            professors.push(this.generateProfessor());
+        }
+        return professors;
+    }
+    generateStudent(): Student { return new Student(names[Random.randomInteger(0, names.length)]); }
+    generateStudents(arrayLength: number): Student[] {
+        if (ValidationHelper.isNonPositive(arrayLength)) { throw new Error("Amount of students must be greater than zero."); }
+        let students: Student[] = [];
+        for (let i = 0; i < arrayLength; i++) {
+            students.push(this.generateStudent());
+        }
+        return students;
+    }
+    generateStaff(): Staff { return new Staff(names[Random.randomInteger(0, names.length)]); }
+    generateStaffs(arrayLength: number) {
+        if (ValidationHelper.isNonPositive(arrayLength)) { throw new Error("Amount of staffs must be greater than zero."); }
+        let staffs: Staff[] = [];
+        for (let i = 0; i < arrayLength; i++) {
+            staffs.push(this.generateStaff());
+        }
+        return staffs;
+    }
+    imitateYearCycle(workers: Person[]) {
+        const workingDay: number = 8;
+        for (let i = 0; i < 12; i++) {
+            for (let j = 0; j < 31; j++) {
+                workers.forEach((worker) => {
+                    worker.addHours(workingDay + Random.randomInteger(-1, 3));
+                });
+            }
+            workers.forEach((worker) => {
+                worker.receiveSalary(worker.calculateSalary(worker.hours));
+                worker.resetHours();
+                console.log(`${worker.name} balance for now - ${worker.balance}`);
+
+            });
+            console.log("-======================-");
+        }
+    }
+
+}
+// -==main==-
+const imitator = new LifeImitator();
+imitator.imitateYearCycle([...imitator.generateProfessors(3), ...imitator.generateStudents(10), ...imitator.generateStaffs(3)]);
+
+
+
---
+++ src/names.ts
@@ -0,0 +1,7 @@
+export const names: string[] = [
+    "Alex", "Max", "John", "Mike", "Chris", "Daniel", "Leo", "Nick", "Ryan", "Ethan", "Oliver", "Jacob", "Lucas",
+    "Noah", "James", "Adam", "Ben", "Sam", "Tom", "Jack", "Oscar", "Henry", "Arthur", "Theo", "Logan", "Mason",
+    "Dylan", "Caleb", "Aaron", "Nathan", "Isaac", "Elijah", "Sebastian", "Connor", "Hunter", "Parker", "Cole",
+    "Blake", "Jordan", "Tyler", "Kevin", "Brandon", "Justin", "Eric", "Scott", "Brian", "Jason", "Mark", "Paul",
+    "Luke", "Matt",
+]
